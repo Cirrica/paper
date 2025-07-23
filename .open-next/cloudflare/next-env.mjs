ad0d52ff90ef26c93c18dc8a5f76f6ecfd979ab4
anthonyk2923 <anthony@fprintf.com>
2025-07-22 19:05:23 -0500

    opennext

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_API_URL":"https://api.cirrica.com","NEXT_PUBLIC_PAPER_URL":"http://localhost:3001"};
+export const development = {"NEXT_PUBLIC_API_URL":"https://api.cirrica.com","NEXT_PUBLIC_PAPER_URL":"http://localhost:3001"};
+export const test = {"NEXT_PUBLIC_API_URL":"https://api.cirrica.com","NEXT_PUBLIC_PAPER_URL":"http://localhost:3001"};
