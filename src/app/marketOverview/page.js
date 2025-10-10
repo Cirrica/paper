d0b85b7a1179403df342bf49a4f853b9297406b6
anthonyk2923 <anthony@fprintf.com>
2025-10-10 08:40:09 -0500

    fixed import

--- src/app/marketOverview/page.js
+++ src/app/marketOverview/page.js
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import DashboardCard from "./DashboardCard";
-import HoldingsTable from "./HoldingsTable";
-import TradesTable from "./TradesTable";
-import Notifications from "./Notifications";
-import Sidebar from "../components/sidebar";
-import ProfileHeader from "../components/profileHeader";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import DashboardCard from './DashboardCard';
+import HoldingsTable from './HoldingsTable';
+import TradesTable from './TradesTable';
+import Notifications from './Notifications';
+import Sidebar from '../components/Sidebar';
+import ProfileHeader from '../components/profileHeader';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function MarketOverview() {
   const [token, setToken] = useState(null);
@@ -18,45 +18,45 @@ export default function MarketOverview() {
   //Authentication
 
   useEffect(() => {
-    const storedToken = localStorage.getItem("cirricaToken");
+    const storedToken = localStorage.getItem('cirricaToken');
 
     if (storedToken) {
       setToken(storedToken);
       setIsAuthenticated(true);
     } else {
-      router.push("/");
+      router.push('/');
     }
   }, [router]);
 
   const handleLogout = () => {
-    localStorage.removeItem("cirricaToken");
+    localStorage.removeItem('cirricaToken');
     setToken(null);
     setIsAuthenticated(false);
-    router.push("/");
+    router.push('/');
   };
   if (!isAuthenticated) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        {" "}
-        <div className="text-center">
-          {" "}
-          <p>Checking authentication...</p>{" "}
-        </div>{" "}
+      <div className='min-h-screen flex items-center justify-center'>
+        {' '}
+        <div className='text-center'>
+          {' '}
+          <p>Checking authentication...</p>{' '}
+        </div>{' '}
       </div>
     );
   }
 
   return (
-    <div className="flex min-h-screen text-white">
+    <div className='flex min-h-screen text-white'>
       {/* Sidebar + Profile Header */}
 
       <Sidebar />
-      <main className="flex-1 px-8 py-4">
+      <main className='flex-1 px-8 py-4'>
         <ProfileHeader
           user={{
-            name: "James Raymond",
-            accountNumber: "4453728992",
-            profilePicture: "/IconProfilePicture.svg",
+            name: 'James Raymond',
+            accountNumber: '4453728992',
+            profilePicture: '/IconProfilePicture.svg',
           }}
           balances={{
             portfolio: 623098.17,
@@ -66,24 +66,24 @@ export default function MarketOverview() {
 
         {/* Portfolio Dashboard */}
 
-        <h3 className="text-[20px] font-bold pt-4">Portfolio Dashbaord</h3>
-        <div className="flex gap-6 my-8">
-          <button className="px-4 py-2 bg-gold rounded-xl border-t border-l border-r border-gray-500 w-[247px] h-[70px] text-[17px]">
+        <h3 className='text-[20px] font-bold pt-4'>Portfolio Dashbaord</h3>
+        <div className='flex gap-6 my-8'>
+          <button className='px-4 py-2 bg-gold rounded-xl border-t border-l border-r border-gray-500 w-[247px] h-[70px] text-[17px]'>
             Real Trading
           </button>
-          <button className="px-4 py-2 bg-dark-gray rounded-xl border-t border-l border-r border-gray-500 w-[247px] h-[70px] text-[17px]">
+          <button className='px-4 py-2 bg-dark-gray rounded-xl border-t border-l border-r border-gray-500 w-[247px] h-[70px] text-[17px]'>
             Paper Trader
           </button>
         </div>
 
-        <div className="grid grid-cols-4 gap-4 mb-8 pt-2 pb-8">
-          <DashboardCard label="Total Balance" value="$25,400" />
-          <DashboardCard label="Invested Capital" value="$20,000" />
-          <DashboardCard label="P&L Today" value="+300" color="text-positive" />
+        <div className='grid grid-cols-4 gap-4 mb-8 pt-2 pb-8'>
+          <DashboardCard label='Total Balance' value='$25,400' />
+          <DashboardCard label='Invested Capital' value='$20,000' />
+          <DashboardCard label='P&L Today' value='+300' color='text-positive' />
           <DashboardCard
-            label="Overall P&L"
-            value="+5,400"
-            color="text-positive"
+            label='Overall P&L'
+            value='+5,400'
+            color='text-positive'
           />
         </div>
 
@@ -91,11 +91,11 @@ export default function MarketOverview() {
         <HoldingsTable />
         <TradesTable />
 
-        <div className="grid grid-cols-4 gap-4 mb-8 pb-8">
-          <DashboardCard label="Win rate" value="72%" />
-          <DashboardCard label="Avg Holding Time" value="5 Days" />
-          <DashboardCard label="Sharpe Ratio" value="1.24" />
-          <DashboardCard label="vs SPY" value="+5.3%" color="text-positive" />
+        <div className='grid grid-cols-4 gap-4 mb-8 pb-8'>
+          <DashboardCard label='Win rate' value='72%' />
+          <DashboardCard label='Avg Holding Time' value='5 Days' />
+          <DashboardCard label='Sharpe Ratio' value='1.24' />
+          <DashboardCard label='vs SPY' value='+5.3%' color='text-positive' />
         </div>
 
         <Notifications />
